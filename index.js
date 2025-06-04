274dfca32cdfe46e56f1b82021fd6f938817e2c2
Sushil Kumar Mishra <131292329+Sushil2k4@users.noreply.github.com>
2025-06-05 00:38:08 +0530

    Added code for filters in JS

--- index.js
+++ index.js
@@ -4,8 +4,38 @@ const ans = input.map(function (element) {
     return element*2;
 });
 console.log(ans);
+
 //short way 
+
 const CustomMap = (arr, transfromFnc) => arr.map(transfromFnc);
 const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const output = CustomMap(input, x => x * 5);
 console.log(output);
+
+
+//filterssssssssssssssssss
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const newArr = [];
+
+for (let i=0; i<arr.length; i++) {
+    if (arr[i]%2 == 0) {
+        newArr.push(arr[i]);
+    }
+}
+
+console.log(newArr);
+
+//short way
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function filterLogic(n) {
+    if ( n % 2 == 0 ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const ans = arr.filter(filterLogic);
+console.log(ans);
